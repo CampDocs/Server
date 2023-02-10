5fe35cfd13aca1034dc8d7fd8b78e3642b303bb1
João Pedro Lopes <joaopalopes24@gmail.com>
2023-02-09 22:18:58 -0300

    refactor(auth): remove the register function in system

--- routes/auth.php
+++ routes/auth.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/auth')->group(function () {
     Route::middleware(['guest'])->group(function () {
         Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-
-        Route::post('/register', RegisteredUserController::class);
 
         Route::post('/forgot-password', PasswordResetLinkController::class);
 
